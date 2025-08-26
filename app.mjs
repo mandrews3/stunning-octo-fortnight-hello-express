11f34a0b27eaea57e0162d86e0f4baf87a8191eb
Cumbie <bcumbie@una.edu>
2025-08-26 14:00:27 -0500

    adjfaksl

--- app.mjs
+++ app.mjs
@@ -1,18 +1,37 @@
 import express from 'express'
+import path from 'path';
+// import { dirname } from 'node:path';
+// import { fileURLToPath } from 'node:url';
 
 const app = express()
-
 const PORT = process.env.PORT || 3000; 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+
+
+// app.use(express.static(__dirname + 'public'));
+
 
 app.get('/', (req, res) => {
-  res.send('Hello Express from Render 😍😍😍')
+  res.send('Hello Express from Render 😍😍😍. <a href="barry">barry</a>')
 })
 
 // endpoints...middlewares...apis? 
 // send an html file
 
-// app.listen(3000)
-// TODO: refactor to use env port. 
+app.get('/barry', (req, res) => {
+  // res.send('barry. <a href="/">home</a>')
+
+  res.sendFile('barry.html'); 
+
+})
+
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
