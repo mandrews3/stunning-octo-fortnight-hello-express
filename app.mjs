0e943be5f229ca76711e5e8c71fe24b8371fea8f
Cumbie <bcumbie@una.edu>
2025-08-26 13:20:04 -0500

    refactor port

--- app.mjs
+++ app.mjs
@@ -2,8 +2,17 @@ import express from 'express'
 
 const app = express()
 
+const PORT = process.env.PORT || 3000; 
+
 app.get('/', (req, res) => {
-  res.send('Hello Express')
+  res.send('Hello Express from Render 😍😍😍')
 })
 
-app.listen(3000)
+// endpoints...middlewares...apis? 
+// send an html file
+
+// app.listen(3000)
+// TODO: refactor to use env port. 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
